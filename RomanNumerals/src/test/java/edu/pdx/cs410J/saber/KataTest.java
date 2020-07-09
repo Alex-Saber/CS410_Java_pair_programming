53f35ac55076d7393cb30f332a625879170cefa4
asaber <alex.saber@dascena.com>
2020-07-08 19:52:13 -0700

    Adds simple test

--- RomanNumerals/src/test/java/edu/pdx/cs410J/saber/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/saber/KataTest.java
@@ -2,12 +2,20 @@ package edu.pdx.cs410J.saber;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
   @Test
   public void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  public void romanNumeralFor1IsI() {
+    assertThat(Kata.romanNumeralFor(1), equalTo("I"));
   }
 
 }
